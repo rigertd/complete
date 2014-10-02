0d9d29b93e6da63181c5cdef0939c3f61c517e5a
David Rigert <rigertd@onid.orst.edu>
2014-10-01 22:01:56 -0700

    Created initial version of echo.cpp

---
+++ assignment1/echo.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+ * Author:                 David Rigert
+ * Date Created:           10/1/2014
+ * Last Modification Date: 10/1/2014
+ * Assignment:             Assignment 1
+ * Filename:               echo.cpp
+ *********************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long input; // for storing user input
+	
+	cout << "Enter an integer value for me to repeat: ";
+	cin >> input;
+	cout << "\nYou entered " << input << ".\n";
+	
+	return 0;
+}
+	
