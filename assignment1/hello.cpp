d37c6fdd47c5e07f5aa7c65995e533706ba8ea0e
David Rigert <rigertd@onid.orst.edu>
2014-10-01 21:52:37 -0700

    Added main documentation header block to hello.cpp.

--- assignment1/hello.cpp
+++ assignment1/hello.cpp
@@ -1,3 +1,21 @@
+/**********************************************************
+* Author:                 David Rigert
+* Date Created:           10/1/2014
+* Last Modification Date: 10/1/2014
+* Assignment:             Assignment 1
+* Filename:               hello.cpp
+*
+* Overview:
+*     This program outputs "Hello World!" to the console.
+* 
+* Input:
+*     None.
+*
+* Output:
+*     The output to the console will be in the form of:
+*
+*     Hello World!
+**********************************************************/
 #include <iostream>
 using namespace std;
 
