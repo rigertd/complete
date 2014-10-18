f4ee7cea7f8da366bbdd6f2e1dcced4354303368
David Rigert <rigertd@onid.orst.edu>
2014-10-17 17:27:08 -0700

    Initial version of assignment 3.

---
+++ assignment3/strings.cpp
@@ -0,0 +1,35 @@
+/**********************************************************
+* Author:                 David Rigert
+* Date Created:           10/9/2014
+* Last Modification Date: 10/9/2014
+* Assignment:             Assignment 2
+* Filename:               arcade.cpp
+*
+* Overview:
+*     This program prompts the user for the number of coupons
+*     and prints how many candy bars and gumballs the coupons
+*     can be exchanged for. It also prints how many coupons will
+*     be left over.
+*     This version of the program uses division and remainders
+*     to determine the number of each prize.
+* 
+* Input:
+*     Number of coupons
+*
+* Output:
+*     The output to the console will be in the form of:
+*
+*     Enter the number of coupons you have: 
+*
+*     Your # coupons can be exchanged for:
+*         # candy bars
+*         # gumballs
+*     with # coupons left over.
+**********************************************************/
+#include <string>
+
+int main()
+{
+	string my_str;
+	
+}
