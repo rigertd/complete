69eb13a8e1a1a4294f6b93f3128e7a4bd2466354
David Rigert <rigertd@onid.orst.edu>
2015-05-02 18:13:17 -0700

    Added automatic redirect to content1.php if user is already logged in.

--- assignment4-part1/src/login.php
+++ assignment4-part1/src/login.php
@@ -1,23 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-header('Content-Type: text/html');
 
 session_start();
 
+/* for building URL to current path */
+$host = $_SERVER['HTTP_HOST'];
+$url = rtrim(dirname($_SERVER['PHP_SELF']), "\\/");
+
 /* delete session data if logout action */
 if (isset($_GET) && isset($_GET['action']) && $_GET['action'] === 'logout') {
-    /* for building URL to current path */
-    $host = $_SERVER['HTTP_HOST'];
-    $url = rtrim(dirname($_SERVER['PHP_SELF']), "\\/");
-    
-    if (session_status() === PHP_SESSION_ACTIVE) {
+    if (session_status() == PHP_SESSION_ACTIVE) {
         session_destroy();
     }
     /* redirect without logout action to avoid accidental logout */
     header("Location: http://{$host}{$url}/login.php");
     die();
+} else if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['username'])) {
+    /* user already logged in--redirect to content1.php */
+    header("Location: http://{$host}{$url}/content1.php");
 }
+header('Content-Type: text/html');
 ?>
 <!DOCTYPE html>
 <html>
