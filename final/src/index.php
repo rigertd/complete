3571d86264ae74b6e7c4e7b18b944826d3b24227
David Rigert <rigertd@onid.orst.edu>
2015-05-25 13:21:15 -0700

    Added support for child profiles.

--- final/src/index.php
+++ final/src/index.php
@@ -1,6 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+require_once('dbinfo.php');
+
+include 'dbfuncs.php';
 
 session_start();
 
@@ -18,6 +21,38 @@ if (session_status() != PHP_SESSION_ACTIVE || !isset($_SESSION['email'])){
 $first = $_SESSION['first'];
 $last = $_SESSION['last'];
 $email = $_SESSION['email'];
+
+/* Establish database connection */
+$mysqli = new mysqli(DB_ADDR, DB_USER, DB_PW, DB_NAME);
+if ($mysqli->connect_errno) {
+    echo "Database connection error (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    die();
+}
+
+/**
+ * Gets an array of child profiles associated with the account.
+ * @param mysqli $db         The mysqli database interface.
+ * @param String $user_email The logged in user's email address.
+ * @return array An array of profile {id, name, dob}
+ */
+function getProfiles($db, $user_email) {
+    $stmt = prepareQuery($db, 'SELECT p.id, p.name, p.dob ' .
+                              'FROM Users as u ' .
+                              'INNER JOIN Profiles as p ON u.id = p.parent_id ' .
+                              'WHERE u.email = ? ' .
+                              'ORDER BY p.name ASC');
+    bindParam($stmt, "s", $user_email);
+    executeStatement($stmt);
+    $results = $stmt->get_result();
+    $profiles = array();
+    while ($row = $results->fetch_assoc()) {
+        $profiles[] = $row;
+    }
+    return $profiles;
+}
+
+$profiles = getProfiles($mysqli, $email);
+
 
 ?>
 <meta charset="utf-8">
@@ -38,6 +73,11 @@ $email = $_SESSION['email'];
       <h2 class="col-sm-9">Baby Growth Tracker</h2>
       <div class="col-sm-3"><a href="login.php?action=logout" class="btn btn-default" role="button">Log Out</a></div>
     </header>
+    <select name="profile">
+<?php foreach ($profiles as $profile): ?>
+      <option value="<?php echo $profile['id']; ?>"><?php echo htmlspecialchars($profile['name'].' ('.$profile['dob'].')') ?></option>
+<?php endforeach ?>
+    </select>
     <p>Hello <?php echo $first ?>, welcome to the dashboard.</p>
     <footer>&copy; David Rigert</footer>
   </div>
