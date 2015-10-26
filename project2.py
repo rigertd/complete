3358ef94e4c13424af5e676a447bf4325c30f8e9
perrotti <perrotti@onid.oregonstate.edu>
2015-10-25 23:00:35 -0700

    Update project2.py
    Fixed a couple of indentation problems, and casted change greedy solution to ints to make it work. All three formulas operating as expected.

--- project2.py
+++ project2.py
@@ -42,15 +42,15 @@ def changegreedy(arr, val):
 	it moves on to the next smallest.
 	"""
 	if arr[0] != 1:
-        raise ValueError("The first element of arr must be 1.")
+		raise ValueError("The first element of arr must be 1.")
 	change = val
 	coin_count = 0
 	coin_arr = [0]*len(arr)
 	
 	for i, x in reversed(list(enumerate(arr))):
 		if x <= change:
-			coin_arr[i] += change / x
-			coin_count += change / x
+			coin_arr[i] += int(change / x)
+			coin_count += int(change / x)
 			change = change % x
 	
 	return coin_arr, coin_count
@@ -64,7 +64,7 @@ def changeslow(arr, val):
 	all possibilities where coin value is less than change left to give. 
 	"""
 	if arr[0] != 1:
-        raise ValueError("The first element of arr must be 1.")
+		raise ValueError("The first element of arr must be 1.")
 	for i, x in reversed(list(enumerate(arr))):
 		if x == val:
 			coin_arr = [0] * len(arr)
@@ -95,28 +95,28 @@ def changedp(arr, val):
 	to reach a certain value between 1...val so that work is not repeated. Removes steps of
 	recursion that are unecessary. 
 	"""
-    if arr[0] != 1:
-        raise ValueError("The first element of arr must be 1.")
-    lookup_table = [([0] * len(arr), 0)]
-    for i in range(1, val + 1):
-        min_count = None
-        coin_idx = None
-        for j, v in enumerate(arr):
-            if v <= i and (min_count == None or lookup_table[i - v][1] + 1 < min_count):
-                min_count = lookup_table[i - v][1] + 1
-                coin_idx = j
+	if arr[0] != 1:
+		raise ValueError("The first element of arr must be 1.")
+	lookup_table = [([0] * len(arr), 0)]
+	for i in range(1, val + 1):
+		min_count = None
+		coin_idx = None
+		for j, v in enumerate(arr):
+			if v <= i and (min_count == None or lookup_table[i - v][1] + 1 < min_count):
+				min_count = lookup_table[i - v][1] + 1
+				coin_idx = j
 
-        # make a copy of counts for current val - coin val
-        count_arr = lookup_table[i - arr[coin_idx]][0][:]
-        count_arr[coin_idx] += 1
-        lookup_table.append((count_arr, min_count))
-    
-    return lookup_table[val]
+		# make a copy of counts for current val - coin val
+		count_arr = lookup_table[i - arr[coin_idx]][0][:]
+		count_arr[coin_idx] += 1
+		lookup_table.append((count_arr, min_count))
+
+	return lookup_table[val]
 
 
 if __name__ == '__main__':
 	v = [1, 3, 7, 12]
-	a = 29
+	a = 31
 	result_arr, result_coin = changegreedy(v, a)
 	print(result_arr)
 	print(result_coin)
