de1099a566fb53d7c46d28eb1b601c771e305a7b
awmcdaniel <awmcdaniel@yahoo.com>
2015-10-27 14:15:47 -0700

    now calls relevant algorithm

--- project2/CoinChangeTimer.py
+++ project2/CoinChangeTimer.py
@@ -8,8 +8,8 @@ CoinChangeTimer -- Determines the running time of the algorithms
 implemented in project2.py
 """
 
-#import time 				# Used for timing calculations
-#import project2				# Used for importing coin change algorithms
+import time 				# Used for timing calculations
+import project2				# Used for importing coin change algorithms
 
 # Ask user which algorithm they want to run. Incorrect input validation takes place later.
 print ("Choose an algorithm first:\n")
@@ -40,12 +40,12 @@ elif inputSelect == 3:
 		changeValues.append(i)
 elif inputSelect == 4:
 	for j in range(0, 30):
-		coinValues.append(j)
+		coinValues.append(j + 1)
 	for i in range(2000, 2201):
 		changeValues.append(i)
 else:	# This is validation that you entered an int between 1-4... otherwise breaks
-	print("You didn't enter a valid int (1-4)")
-	exit("Invalid user input")
+	print("You didn't choose a valid pair (1-4)")
+	exit("Invalid user input - coin values/change")
 
 # trace statements, just checking to be sure the arrays are populating correctly
 # for i in range(0, len(changeValues)):
@@ -53,52 +53,33 @@ else:	# This is validation that you entered an int between 1-4... otherwise brea
 # for i in range(0, len(coinValues)):
 # 	print("Value ",i," = ",coinValues[i],"\n")
 
-# #Initialize the document we will print to 
-# with open('results.csv', 'w') as csvoutput:  
-# 	writer = csv.writer(csvoutput, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-# 	# Write the formula the user chose, and the header for the data we will print
-# 	writer.writerow(["Formula Used:", formulaSelect])
-# 	writer.writerow(["n Value", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+total_time = 0
 
-# 	# We want to run through 10 times for 10 random results
-# 	for x in range(0, len(nValue)):
-# 		# Each new test needs an array to store the time elapsed calculations
-# 		timeList=[]
-# 		# We want 10 results per n value, so have to loop 10 times
-# 		for y in range(0, 10):
-# 			# Generate an array and fill with 'n' random values
-# 			numList=[]
-# 			# Need to create random numbers based on the nValue assigned
-# 			for i in range(0, nValue[x]):
-# 				num = randint(-100,100)
-# 				numList.append(num)
-			
-# 			# Print out results 10 times based on the code provided 
-# 			# Essentially switch statement for using the correct formula
-# 			if formulaSelect == 1:
-# 				start_time = time.time()
-# 				project1.max_subarray_enum(numList)
-# 				elapsed_time = time.time() - start_time
-# 				timeList.append(elapsed_time)
-# 			elif formulaSelect == 2:
-# 				start_time = time.time()
-# 				project1.max_subarray_better_enum(numList)
-# 				elapsed_time = time.time() - start_time
-# 				timeList.append(elapsed_time)
-# 			elif formulaSelect == 3:
-# 				start_time = time.time()
-# 				project1.max_subarray_divide_and_conquer(numList)
-# 				elapsed_time = time.time() - start_time
-# 				timeList.append(elapsed_time)
-# 			elif formulaSelect == 4:
-# 				start_time = time.time()
-# 				project1.max_subarray_dynamic(numList)
-# 				elapsed_time = time.time() - start_time
-# 				timeList.append(elapsed_time)
-# 			else:	# This is validation that you entered an int between 1-4... otherwise breaks
-# 				print("You didn't enter a valid int (1-4)")
-# 				exit("Invalid user input")
-		
-# 		# After obtaining timings for all timing values for the n numbers, we print them to the CSV file
-# 		writer.writerow([nValue[x],timeList[0],timeList[1],timeList[2],timeList[3],timeList[4],timeList[5],timeList[6],timeList[7],timeList[8],timeList[9]])
-# 		timeList[:] = [] 	# Reset timeList variable
+# Call the function for the algorithm the user selected
+if algorithmSelect == 1:
+	start_time = time.time()
+	for value in changeValues:
+		project2.changeslow(coinValues, value)
+		run_time = time.time() - start_time
+		total_time = total_time + run_time
+	print("Brute Force run time:\n")
+	print("Input pair ",inputSelect,"took: ",total_time,"\n")
+elif algorithmSelect == 2:
+	start_time = time.time()
+	for value in changeValues:
+		project2.changegreedy(coinValues, value)
+		run_time = time.time() - start_time
+		total_time = total_time + run_time
+	print("Greedy run time:\n")
+	print("Input pair ",inputSelect,"took: ",total_time,"\n")
+elif algorithmSelect == 3:
+	start_time = time.time()
+	for value in changeValues:
+		project2.changedp(coinValues, value)
+		run_time = time.time() - start_time
+		total_time = total_time + run_time
+	print("Dynamic Programming run time:\n")
+	print("Input pair ",inputSelect,"took: ",total_time,"\n")
+else:	# This is validation that you entered an int between 1-3... otherwise breaks
+	print("You didn't choose a valid algorithm (1-3)")
+	exit("Invalid user input - algorithm")
