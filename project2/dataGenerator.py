0208d1e696ed8ea90653a4a4ad089222752a73af
David Rigert <rigertd@onid.orst.edu>
2015-10-28 14:08:24 -0700

    Generated results on Flip.

--- project2/dataGenerator.py
+++ project2/dataGenerator.py
@@ -19,16 +19,16 @@ for i in range(1, 31):
 
 MAX_A_VAL = 10000
 
-# Run greedy and dp on entire range, but brute force only up to A = 40 and n = 7
+# Run greedy and dp on entire range, but brute force only up to A = 40
 with open('brute_results.csv', 'w') as csvoutput:
     writer = csv.writer(csvoutput, delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
     writer.writerow(['', 'Denomination Count'])
     writer.writerow(['A'] + list(range(1, 31)))
-    for A in range(1, 40):
+    for A in range(1, 41):
         print('A={}'.format(A))
         row = [A]
-        for coin_set in coin_sets[:7]:
-            print('n={}'.format(len(coin_set)))
+        for coin_set in coin_sets:
+            #print('n={}'.format(len(coin_set)))
             start_time = time.time()
             project2.changeslow(coin_set, A)
             run_time = time.time() - start_time
@@ -39,11 +39,11 @@ with open('greedy_results.csv', 'w') as csvoutput:
     writer = csv.writer(csvoutput, delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
     writer.writerow(['', 'Denomination Count'])
     writer.writerow(['A'] + list(range(1, 31)))
-    for A in range(1, MAX_A_VAL):
+    for A in range(100, MAX_A_VAL, 100):
         print('A={}'.format(A))
         row = [A]
         for coin_set in coin_sets:
-            print('n={}'.format(len(coin_set)))
+            #print('n={}'.format(len(coin_set)))
             start_time = time.time()
             project2.changegreedy(coin_set, A)
             run_time = time.time() - start_time
@@ -54,13 +54,14 @@ with open('dp_results.csv', 'w') as csvoutput:
     writer = csv.writer(csvoutput, delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
     writer.writerow(['', 'Denomination Count'])
     writer.writerow(['A'] + list(range(1, 31)))
-    for A in range(1, MAX_A_VAL):
+    for A in range(100, MAX_A_VAL, 100):
         print('A={}'.format(A))
         row = [A]
         for coin_set in coin_sets:
-            print('n={}'.format(len(coin_set)))
+            #print('n={}'.format(len(coin_set)))
             start_time = time.time()
             project2.changedp(coin_set, A)
             run_time = time.time() - start_time
             row.append(run_time)
         writer.writerow(row)
+
