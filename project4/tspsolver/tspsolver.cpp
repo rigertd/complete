d0f8414bb4175c924c9f7ce50dc9b150594150f0
David Rigert <rigertd@onid.orst.edu>
2015-11-28 12:23:09 -0800

    Added multithreading to brute force approach.

--- project4/tspsolver/tspsolver.cpp
+++ project4/tspsolver/tspsolver.cpp
@@ -10,44 +10,108 @@
 #include <set>
 #include <algorithm>
 #include <iterator>
+#include <queue>
+#include <thread>
+
+typedef unsigned int uint;
 
 class TspSolver {
 private:
 	struct Distance {
-		unsigned int destId;
-		unsigned int val;
+		uint destId;
+		uint val;
+	};
+
+	struct CompareDistance {
+		bool operator()(Distance const& lhs, Distance const& rhs) {
+			return lhs.val > rhs.val;
+		}
 	};
 
 	struct City {
-		unsigned int id;
-		unsigned int x;
-		unsigned int y;
-		std::vector<Distance> dists;
+		uint id;
+		uint x;
+		uint y;
+		std::priority_queue<Distance, std::vector<Distance>, CompareDistance> dists;
 	};
 
-	void findShortestRecursive(std::vector<City>& notVisited, std::vector<unsigned int>& path, unsigned int& totalDist) {
-		if (notVisited.size() > 0) {
+	void computeDistances() {
+		size_t count = cities.size();
+		size_t row, col;
 
+		for (size_t i = 0; i < count; ++i) {
+			for (size_t j = i; j < count; ++j) {
+				row = cities[i].id;
+				col = cities[j].id;
+				if (i != j) {
+					distances[row][col] = distances[col][row] = static_cast<uint>(std::sqrt(
+						cities[i].x * cities[i].x +
+						cities[j].y * cities[j].y));
+				}
+				else {
+					distances[row][col] = 0;
+				}
+			}
 		}
-		else {
-			shortestDist = totalDist;
+
+		for (size_t i = 0; i < count; ++i) {
+			for (size_t j = 0; j < count - 1; ++j) {
+				if (i != j) {
+					Distance d;
+					d.destId = cities[j].id;
+					d.val = distances[i][j];
+					cities[i].dists.push(d);
+				}
+			}
 		}
 	}
 
+	void findShortestRecursive(City current, std::stack<uint>& path, std::set<uint> visited, uint totalDist) {
+		while (current.dists.size() > 0) {
+			Distance d = current.dists.top();
+			current.dists.pop();
+			if (visited.find(d.destId) == visited.end()) {
+				visited.insert(d.destId);
+				totalDist += d.val;
+				if (totalDist < shortestDist) {
+					findShortestRecursive(cities[d.destId], path, visited, totalDist + d.val);
+				}
+				else {
+					break;
+				}
+				
+			}
+		}
+		if (current.dists.size() == 0) {
+			shortestDist = shortestDist > totalDist ? totalDist : shortestDist;
+			std::cout << "Reached end of path, distance: " << totalDist << std::endl;
+		}
+	}
+
+	// allocate memory for a 2d array to store distance values, initialized to max uint
 	void initializeDistances() {
-		distances = new unsigned int*[cities.size()];
-		for (int i = 0; i < cities.size(); ++i) {
-			distances[i] = new unsigned int[cities.size()];
+		uint maxInt = std::numeric_limits<uint>::max();
+
+		distances = new uint*[cities.size()];
+		for (size_t i = 0, ilen = cities.size(); i < ilen; ++i) {
+			distances[i] = new uint[ilen];
+			for (size_t j = 0; j < ilen; ++j) {
+				distances[i][j] = maxInt;
+			}
 		}
+		isInitialized = true;
 	}
 
+	// free memory allocated to distances array
 	void finalizeDistances() {
-		for (int i = 0; i < cities.size(); ++i) {
+		for (size_t i = 0, ilen = cities.size(); i < ilen; ++i) {
 			delete[] distances[i];
 		}
 		delete[] distances;
+		isInitialized = false;
 	}
 
+	// sorting comparison functions
 	static bool sortById(const City& lhs, const City& rhs) {
 		return lhs.id < rhs.id;
 	}
@@ -56,82 +120,61 @@ private:
 		return lhs.val < rhs.val;
 	}
 
-	unsigned int** distances;
+	uint** distances;
 	std::vector<City> cities;
-	unsigned int shortestDist;
-	unsigned int minId;
-	unsigned int maxId;
-	std::string path;
+	uint shortestDist;
+	uint minId;
+	uint maxId;
+	bool isInitialized;
 
 public:
 	TspSolver() {
 		// initialize minId and shortestDist to max value
-		minId = shortestDist = std::numeric_limits<unsigned int>::max();
+		minId = shortestDist = std::numeric_limits<uint>::max();
 		// initialize maxId to smallest value
 		maxId = 0;
+		isInitialized = false;
 	}
 
 	~TspSolver() {
-		if (cities.size() > 0) {
+		if (isInitialized) {
 			finalizeDistances();
 		}
 	}
 
-	void computeDistances() {
-		unsigned int count = cities.size();
-		unsigned int row, col;
+	void findShortest() {
+		std::set<uint> visited;
+		std::stack<uint> path;
+		uint maxThreads = std::thread::hardware_concurrency();
+		std::vector<std::thread*> threads;
 
-		initializeDistances();
+		maxThreads = maxThreads < 2 ? 2 : maxThreads;
 
-		for (int i = 0; i < count; ++i) {
-			for (int j = i; j < count; ++j) {
-				row = cities[i].id;
-				col = cities[j].id;
-				if (i != j) {
-					distances[row][col] = distances[col][row] = std::sqrt(
-						cities[i].x * cities[i].x +
-						cities[j].y * cities[j].y);
+		City start = cities[0];
+		visited.insert(start.id);
+		while (!start.dists.empty()) {
+			while (threads.size() < maxThreads - 1) {
+				Distance next = start.dists.top();
+				start.dists.pop();
+				std::thread* t = new std::thread(&TspSolver::findShortestRecursive, this, cities[next.destId], path, visited, next.val);
+				threads.push_back(t);
+			}
+			for (size_t i = 0, ilen = threads.size(); i < ilen; ++i) {
+				std::thread* t = threads[i];
+				if (t->joinable()) {
+					t->join();
+					delete t;
 				}
-				else {
-					distances[row][col] = 0;
-				}
+					
 			}
 		}
-
-		for (int i = 0; i < count; ++i) {
-			for (int j = 0; j < count - 1; ++j) {
-				if (i != j) {
-					Distance d;
-					d.destId = cities[i].id;
-					d.val = distances[i][j];
-					cities[i].dists.push_back(d);
-				}
-			}
-			std::sort(cities[i].dists.begin(), cities[i].dists.end(), TspSolver::sortByDestDistance);
-		}
-	}
-
-	void findShortest(std::vector<City>& cities, std::vector<std::vector<unsigned int>> distances) {
-		std::stack<unsigned int> waiting;
-		std::set<unsigned int> visited;
-		std::vector<unsigned int> path;
-
-		waiting.push(cities[0].id);
-		while (waiting.size() > 0) {
-			for (int i = 1; i < cities.size(); ++i) {
-				if (visited.find(cities[i].id) != visited.end()) {
-					waiting.push(cities[i].id);
-
-				}
-			}
-		}
-
+		std::cout << "Shortest path is " << shortestDist << std::endl;
 
 	}
 
 	std::vector<City> getCities() {
 		std::vector<City> updated(cities.size());
-		for (int i = 0; i < cities.size(); ++i) {
+		for (size_t i = 0, ilen = cities.size(); i < ilen; ++i) {
 			City c;
 			c.id = cities[i].id + minId;
 			updated.push_back(c);
@@ -139,10 +182,13 @@ public:
 		return updated;
 	}
 
-	void load(std::string path) {
+	void load(const char* path) {
 		std::ifstream ifs(path);
-
 		if (ifs) {
+			if (isInitialized) {
+				finalizeDistances();
+			}
+
 			std::string buffer;
 
 			while (std::getline(ifs, buffer)) {
@@ -162,16 +208,21 @@ public:
 
 			// set IDs to be 0-based if not already
 			if (minId > 0) {
-				for (int i = 0; i < cities.size(); ++i) {
+				for (size_t i = 0, ilen = cities.size(); i < ilen; ++i) {
 					cities[i].id = cities[i].id - minId;
 				}
 			}
 
 			// sort IDs in ascending order
 			std::sort(cities.begin(), cities.end(), sortById);
+
+			// precalculate all distances between cities
+			initializeDistances();
+			computeDistances();
 		}
 		else {
-			throw "Invalid path specified: " + path;
+			std::cout << "Invalid path specified: " << path << std::endl;
+			exit(1);
 		}
 	}
 };
@@ -185,17 +236,12 @@ int main(int argc, char** argv) {
 
 	TspSolver solver;
 
-	// Check if argument is valid.
-	try {
-		solver.load(std::string(argv[1]));
-	}
-	catch (std::string message) {
-		std::cout << message << std::endl;
-		return 1;
-	}
+	// Try to load the data.
+	solver.load(argv[1]);
 
-	solver.computeDistances();
+	solver.findShortest();
 
+	std::cout << "Done. Press Enter to continue.";
 	std::getchar();
 	return 0;
 }
