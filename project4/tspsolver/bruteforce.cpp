1a56c10370ca5db498e2ebbff47bd408bf315b3d
David Rigert <rigertd@onid.orst.edu>
2015-12-01 00:05:43 -0800

    Updated brute force algorithm to use common functions.

--- project4/tspsolver/bruteforce.cpp
+++ project4/tspsolver/bruteforce.cpp
@@ -1,30 +1,13 @@
-#include <cmath>
-#include <fstream>
 #include <iostream>
-#include <limits>
-#include <string>
-#include <sstream>
-#include <thread>
-#include <vector>
+#include <map>
 #include <set>
 #include <stack>
+#include <thread>
 #include <queue>
-#include <map>
 
-#ifndef nullptr
-#define nullptr NULL
-#endif
+#include "common.h"
 
-typedef unsigned int uint;
-typedef uint uint;
-
-// stores city info from the file
-struct City {
-	uint id;
-	uint x;
-	uint y;
-};
-
+namespace tsp { namespace bruteforce {
 // for sorting children nodes by distance
 struct ChildNode {
 	uint id;
@@ -38,69 +21,13 @@ struct CompareDist {
 };
 
 // globals
-std::map<uint, City> cities;							// stores all of the cities
-uint** distances = nullptr;								// stores the distances between the cities
-uint maxId = 0;											// stores the maximum ID value
 uint shortestSoFar = std::numeric_limits<uint>::max();	// stores the shortest path found so far
 std::map<uint, std::stack<uint>> solutions;				// stores the solutions sorted by distance
 
-void load(const char* path) {
-	std::ifstream ifs(path);
-
-	// load data from file and store in vector buffer
-	if (ifs) {
-		std::string lineBuffer;
-		maxId = 0;
-
-		while (std::getline(ifs, lineBuffer)) {
-			if (!lineBuffer.empty()) {
-				std::istringstream iss(lineBuffer);
-				City c;
-				iss >> c.id;
-				iss >> c.x;
-				iss >> c.y;
-				cities[c.id] = c;
-				maxId = c.id > maxId ? c.id : maxId;
-			}
-		}
-	}
-	else {
-		std::cout << "Invalid path specified: " << path << std::endl;
-		exit(1);
-	}
-}
-
-void computeDists() {
-	distances = new uint*[maxId + 1];
-	for (uint i = 0; i <= maxId; ++i) {
-		distances[i] = new uint[maxId + 1];
-		for (uint j = 0; j <= maxId; ++j) {
-			if (cities.find(i) != cities.end() && cities.find(j) != cities.end()) {
-				if (i < j) {
-					distances[i][j] = static_cast<uint>(
-						std::round(
-							std::sqrt(
-								(cities[i].x - cities[j].x)*(cities[i].x - cities[j].x) +
-								(cities[i].y - cities[j].y)*(cities[i].y - cities[j].y)
-								)
-							)
-						);
-				}
-				else if (j < i) {
-					distances[i][j] = distances[j][i];
-				}
-				else { // i == j
-					distances[i][j] = 0;
-				}
-			}
-		}
-	}
-}
-
-void solveBruteForcePruningRecursive(uint current, std::set<ChildNode, CompareDist> unvisited, uint distToCurrent, std::stack<uint> &path) {
-	path.push(current);
+void solveBruteForcePruningRecursive(City* current, std::set<ChildNode, CompareDist> unvisited, uint distToCurrent, std::stack<uint> &path) {
+	path.push(current->id);
 	if (unvisited.empty()) {
-		uint totalDistance = distToCurrent + distances[current][0];
+		uint totalDistance = distToCurrent + getDistance(current, &cities[0]);
 		if (totalDistance < shortestSoFar) {
 			shortestSoFar = totalDistance;
 			solutions[totalDistance] = path;
@@ -119,7 +46,7 @@ void solveBruteForcePruningRecursive(uint current, std::set<ChildNode, CompareDi
 						nextUnvisited.insert(cn);
 					}
 				}
-				solveBruteForcePruningRecursive(it->id, nextUnvisited, distToCurrent + it->distance, path);
+				solveBruteForcePruningRecursive(&cities[it->id], nextUnvisited, distToCurrent + it->distance, path);
 			}
 		}
 	}
@@ -135,8 +62,8 @@ std::stack<uint> solveBruteForcePruning() {
 	std::set<ChildNode, CompareDist> unvisited;
 	for (auto it = ++(cities.begin()); it != cities.end(); ++it) {
 		ChildNode cn;
-		cn.distance = distances[0][it->first];
-		cn.id = it->first;
+		cn.distance = getDistance(&cities[0], &(*it));
+		cn.id = it->id;
 		unvisited.insert(cn);
 		std::stack<uint> path;
 		path.push(0);
@@ -149,7 +76,7 @@ std::stack<uint> solveBruteForcePruning() {
 			for (auto it2 = unvisited.begin(), end2 = unvisited.end(); it2 != end2; ++it2) {
 				if (it != it2) {
 					ChildNode cn;
-					cn.distance = distances[it->id][it2->id];
+					cn.distance = getDistance(&cities[it->id], &cities[it2->id]);
 					cn.id = it2->id;
 					nextUnvisited.insert(cn);
 				}
@@ -206,38 +133,26 @@ void freeDists() {
 	distances = nullptr;
 }
 
+} }
+
 int main(int argc, char** argv) {
 	// Check if argument was specified. If not, display usage instructions.
 	if (argc < 2) {
 		std::cout << "Usage:\n  tspsolver.exe <input_file>\n\n";
 		return 1;
 	}
-	load(argv[1]);
-	computeDists();
+	tsp::load(argv[1]);
 
-	std::stack<uint> path = solveBruteForcePruning();
-	path = reversePath(path);
+	std::stack<uint> path = tsp::bruteforce::solveBruteForcePruning();
+	path = tsp::bruteforce::reversePath(path);
+	std::vector<uint> tour(path.size());
 
-	char outFilePath[1024];
-	snprintf(outFilePath, sizeof outFilePath, "%s%s", argv[1], ".tour");
-	std::ofstream ofs(outFilePath);
-
-	ofs << shortestSoFar << std::endl;
-
-	std::cout << "Shortest Path: " << path.top();
-	ofs << path.top() << std::endl;
-	path.pop();
 	while (!path.empty()) {
-		uint id = path.top();
+		tour.push_back(path.top());
 		path.pop();
-		std::cout << ", " << id;
-		ofs << path.top() << std::endl;
 	}
 
-	ofs.flush();
-	ofs.close();
+	tsp::save(argv[1], tsp::bruteforce::shortestSoFar, tour);
 
-	freeDists();
-
-	std::getchar();
+	tsp::finalize();
 }
